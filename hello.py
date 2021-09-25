81b132cfa925eb6102b5b2f8eb8ce1fa71461d6b
mrks1998 <39960410+mrks1998@users.noreply.github.com>
2021-09-25 18:05:49 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,3 +5,10 @@ def index():
  return '<h1>Hello Mohammadreza!</h1>'
 if __name__ == '__main__':
  app.run(debug=True)
+
+ @app.route('/')
+def index():
+ return render_template('index.html')
+@app.route('/user/<name>')
+def user(name):
+ return render_template('user.html', name=name)
