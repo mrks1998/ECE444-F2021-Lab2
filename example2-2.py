2c2ba3887284a0adb2bb478be5a709e6dc557234
mrks1998 <39960410+mrks1998@users.noreply.github.com>
2021-09-25 17:58:25 -0400

    Update example2-2.py

--- example2-2.py
+++ example2-2.py
@@ -1,5 +1,12 @@
-from flask import Flask
+   
+from flask import Flask, render_template
 app = Flask(__name__)
+@app.route('/')
+def index():
+ return '<h1>Hello Mohammadreza!</h1>'
+if __name__ == '__main__':
+ app.run(debug=True)
+
 @app.route('/')
 def index():
  return '<h1>Hello Mohammadreza!</h1>'
