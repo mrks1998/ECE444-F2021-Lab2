9087313b5e235b4b6afc7d64b5a14e46a8225906
mrks1998 <39960410+mrks1998@users.noreply.github.com>
2021-09-25 18:10:03 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
-@app.route('/')
-def index():
- return '<h1>Hello Mohammadreza!</h1>'
-if __name__ == '__main__':
- app.run(debug=True)
+#app = Flask(__name__)
+#@app.route('/')
+#def index():
+# return '<h1>Hello Mohammadreza!</h1>'
+#if __name__ == '__main__':
+# app.run(debug=True)
 
  @app.route('/')
 def index():
