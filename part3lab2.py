4ee6cc4c33b56a8eb6448068a7616c9c717fdfca
mrks1998 <39960410+mrks1998@users.noreply.github.com>
2021-09-25 18:00:21 -0400

    Update part3lab2.py

--- part3lab2.py
+++ part3lab2.py
@@ -1,6 +1,16 @@
+from flask import Flask, render_template
+app = Flask(__name__)
 @app.route('/')
 def index():
- return render_template('index.html')
+ return '<h1>Hello Mohammadreza!</h1>'
+if __name__ == '__main__':
+ app.run(debug=True)
+
+@app.route('/')
+def index():
+ return '<h1>Hello Mohammadreza!</h1>'
 @app.route('/user/<name>')
 def user(name):
- return render_template('user.html', name=name)
+ return '<h1>Hello, %s!</h1>' % name
+if __name__ == '__main__':
+ app.run(debug=True)
