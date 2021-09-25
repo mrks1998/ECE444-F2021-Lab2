9653e0c48fff18ed34b72fce6de00e6fd44d611c
mrks1998 <39960410+mrks1998@users.noreply.github.com>
2021-09-25 17:10:39 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
- return '<h1>Hello World!</h1>'
+ return '<h1>Hello Mohammadreza!</h1>'
 if __name__ == '__main__':
  app.run(debug=True)
