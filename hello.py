8d2357b0e9891268bb0b01326b2e8fb88245ed6b
mrks1998 <39960410+mrks1998@users.noreply.github.com>
2021-09-26 12:56:46 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,18 +1,8 @@
-from flask import Flask, render_template
+from flask import render_template
+from app import app
 
-app = Flask(__name__)
 @app.route('/')
+@app.route('/index')
 def index():
- return '<h1>Hello World!</h1>'
-@app.route('/user/<name>')
-def user(name):
- return '<h1>Hello, %s!</h1>' % name
-if __name__ == '__main__':
- app.run(debug=True)
-
- @app.route('/')
-def index():
- return render_template('index.html')
-@app.route('/user/<name>')
-def user(name):
- return render_template('user.html', name=name)
+    user = {'username': 'Miguel'}
+    return render_template('index.html', title='Home', user=user)
